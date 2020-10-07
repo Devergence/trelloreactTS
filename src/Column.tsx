cd53c1df98aec462cee5cb91cbeac171e6bc8986
githubloverforever <dmitry.prihodko@rhinoda.com>
2020-10-08 00:09:41 +0600

    Added components.

---
+++ src/Column.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ColumnContainer, ColumnTitle } from "./styles";
+import {AddNewItem} from "./AddNewItem";
+
+interface ColumnProps {
+  text: string
+}
+
+export const Column = ({text, children}: React.PropsWithChildren<ColumnProps>) => {
+  return (
+    <ColumnContainer>
+      <ColumnTitle>{text}</ColumnTitle>
+      {children}
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} dark/>
+    </ColumnContainer>
+  );
+};
